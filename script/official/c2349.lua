9943ee86c70720d3f5979b412e350a1eb6277600
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-05 16:25:09 -0500

    Update c2349.lua

--- script/official/c2349.lua
+++ script/official/c2349.lua
@@ -1,21 +1,14 @@
 --Guardian Fantasmal
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Definición de atributos
-    c:EnableReviveLimit()
-    c:SetUniqueOnField(1,0,id)
-    c:SetAttribute(ATTRIBUTE_DARK)
-    c:SetLevel(4)
-
-    -- Efecto de no ser destruido en batalla
+    --No puede ser destruido en batalla por monstruos con ATK original menor
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_INDESTRUCTIBLE_BATTLE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
     e1:SetValue(s.indes)
     c:RegisterEffect(e1)
 end
 
--- Función para el efecto de no ser destruido en batalla
 function s.indes(e,c)
-    return c:GetBaseAttack()<=e:GetHandler():GetAttack()
+    return c:GetBaseAttack()<e:GetHandler():GetAttack()
 end
