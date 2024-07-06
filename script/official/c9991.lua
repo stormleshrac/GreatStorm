32c943a3e99cd6a0e740c7ea0911e66527c247be
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-05 22:41:45 -0500

    Update c9991.lua

--- script/official/c9991.lua
+++ script/official/c9991.lua
@@ -40,7 +40,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     e2:SetType(EFFECT_TYPE_FIELD)
     e2:SetCode(EFFECT_UPDATE_ATTACK)
     e2:SetTargetRange(LOCATION_MZONE,0)
-    e2:SetValue(500)
+    e2:SetValue(300)
     e2:SetReset(RESET_PHASE+PHASE_END)
     Duel.RegisterEffect(e2,tp)
 end
