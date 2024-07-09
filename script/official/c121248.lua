86b66bbe05fe8d93ca3b177ef29f708f5258ab5a
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-09 16:13:34 -0500

    Update c121248.lua

--- script/official/c121248.lua
+++ script/official/c121248.lua
@@ -1,76 +1,70 @@
 --Oculus el Majestuoso
 local s,id=GetID()
 function s.initial_effect(c)
-    --Link Summon
+    --Link summon method
     c:EnableReviveLimit()
-    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_FUSION),1,1)
-    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_WARRIOR),1,1)
-    
+    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),2,2,s.lcheck)
     --Negate effect
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_NEGATE)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_CHAINING)
-    e1:SetCountLimit(1)
     e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
     e1:SetCondition(s.negcon)
     e1:SetTarget(s.negtg)
     e1:SetOperation(s.negop)
     c:RegisterEffect(e1)
-    
-    --Direct attack with ATK reduction
+    --Direct attack
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.atkcon)
-    e2:SetOperation(s.atkop)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_DIRECT_ATTACK)
+    e2:SetCondition(s.dircon)
     c:RegisterEffect(e2)
+    --Reduce ATK
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetCondition(s.atkcon)
+    e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
 end
-
+function s.lcheck(g,lc,sumtype,tp)
+    return g:IsExists(Card.IsType,1,nil,TYPE_FUSION,lc,sumtype,tp) and g:IsExists(Card.IsRace,1,nil,RACE_WARRIOR,lc,sumtype,tp)
+end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-    return re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+    return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep~=tp
+        and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
 end
-
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
     Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-    --Prevent attacking this turn
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_CANNOT_ATTACK)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-    e:GetHandler():RegisterEffect(e1)
 end
-
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
-        Duel.Destroy(eg,REASON_EFFECT)
-    end
-end
-
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-    return not e:GetHandler():IsHasEffect(EFFECT_CANNOT_ATTACK)
-end
-
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsFaceup() and c:IsRelateToEffect(e) then
-        --Reduce ATK by 1000
+    if Duel.NegateActivation(ev) then
+        local c=e:GetHandler()
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(-1000)
+        e1:SetCode(EFFECT_CANNOT_ATTACK)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
         c:RegisterEffect(e1)
-        --Direct attack
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_DIRECT_ATTACK)
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        c:RegisterEffect(e2)
     end
+end
+function s.dircon(e)
+    return not e:GetHandler():IsHasEffect(EFFECT_CANNOT_ATTACK)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttackTarget()==nil and e:GetHandler():IsHasEffect(EFFECT_DIRECT_ATTACK)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetValue(-1000)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+    c:RegisterEffect(e1)
 end
