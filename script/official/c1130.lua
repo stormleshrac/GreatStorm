379cdfc25e4e0c84cd71c2c653c75b46ac7e3e70
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 18:30:34 +0200

    Update c1130.lua

--- script/official/c1130.lua
+++ script/official/c1130.lua
@@ -1,19 +1,53 @@
--- Crowa
+--Nueva Calma
 local s,id=GetID()
 function s.initial_effect(c)
-    -- activar en el campo
+    --Activate
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DRAW)
+    e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+    e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+    e:SetLabelObject(g:GetFirst())
 end
+
+function s.costfilter(c)
+    return c:IsRace(RACE_WARRIOR) and c:IsAbleToRemoveAsCost()
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.NegateAttack() end
+end
+
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Draw(tp,2,REASON_EFFECT)
+    if Duel.NegateAttack() then
+        local tc=e:GetLabelObject()
+        if tc and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+            Duel.BreakEffect()
+            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_SET_ATTACK)
+            e1:SetValue(0)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+            tc:RegisterEffect(e1)
+            local e2=Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_SET_DEFENSE)
+            e2:SetValue(0)
+            e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+            tc:RegisterEffect(e2)
+        end
+    end
 end
