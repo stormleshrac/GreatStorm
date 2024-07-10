292067ef038c8c497eb16a1147fbdc46c6bd8c0d
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 10:59:23 -0400

    Update 349500.lua

--- script/official/349500.lua
+++ script/official/349500.lua
@@ -1,12 +1,13 @@
---Nueva Calma
-local s, id = GetID()
+-- Nueva Calma
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Activate
-    local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_NEGATE_ATTACK + CATEGORY_SPECIAL_SUMMON)
+    -- Negate attack and send 1 card to the GY
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_NEGATE_ATTACK+CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetCountLimit(1, id + EFFECT_COUNT_CODE_OATH)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
     e1:SetCondition(s.condition)
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
@@ -14,28 +15,32 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
-function s.condition(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.GetAttacker():IsControler(1 - tp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttacker():IsControler(1-tp)
 end
 
-function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.IsExistingMatchingCard(s.costfilter, tp, LOCATION_HAND, 0, 1, nil) end
-    Duel.DiscardHand(tp, s.costfilter, 1, 1, REASON_COST + REASON_DISCARD)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
+    Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD)
 end
 
-function s.costfilter(c)
-    return c:IsType(TYPE_MONSTER) and c:IsDiscardable()
+function s.cfilter(c)
+    return c:IsAbleToGraveAsCost()
 end
 
-function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 end
-    Duel.SetOperationInfo(0, CATEGORY_NEGATE_ATTACK, nil, 0, 1 - tp, 1)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE_ATTACK,nil,0,1-tp,1)
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 
-function s.activate(e, tp, eg, ep, ev, re, r, rp)
-    Duel.NegateAttack()
-    local tc = Duel.GetOperatedGroup():GetFirst()
-    if tc and tc:IsRace(RACE_WARRIOR) and Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 then
-        Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP_DEFENSE)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateAttack() then
+        local g=Duel.GetOperatedGroup()
+        local tc=g:GetFirst()
+        if tc and tc:IsRace(RACE_WARRIOR) and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+        end
     end
 end
