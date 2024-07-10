b26cb8dba78090021256a61cd627c542e430b86c
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 10:46:39 -0400

    Update 349500.lua

--- script/official/349500.lua
+++ script/official/349500.lua
@@ -6,8 +6,6 @@ function s.initial_effect(c)
     e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
@@ -18,8 +16,10 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
     local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
-    e:SetLabelObject(g:GetFirst())
+    if #g>0 then
+        Duel.Remove(g,POS_FACEUP,REASON_COST)
+        e:SetLabelObject(g:GetFirst())
+    end
 end
 
 function s.costfilter(c)
@@ -28,23 +28,23 @@ end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.NegateAttack() end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     if Duel.NegateAttack() then
         local tc=e:GetLabelObject()
-        if tc and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-            Duel.BreakEffect()
-            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+        if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
             local e1=Effect.CreateEffect(e:GetHandler())
             e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_SET_ATTACK)
+            e1:SetCode(EFFECT_SET_ATTACK_FINAL)
             e1:SetValue(0)
             e1:SetReset(RESET_EVENT+RESETS_STANDARD)
             tc:RegisterEffect(e1)
             local e2=Effect.CreateEffect(e:GetHandler())
             e2:SetType(EFFECT_TYPE_SINGLE)
-            e2:SetCode(EFFECT_SET_DEFENSE)
+            e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
             e2:SetValue(0)
             e2:SetReset(RESET_EVENT+RESETS_STANDARD)
             tc:RegisterEffect(e2)
