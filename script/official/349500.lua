d060ba32de44dd8f979fbfb3b086bd09ac3b6d4e
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 18:06:29 +0200

    Update 349500.lua

--- script/official/349500.lua
+++ script/official/349500.lua
@@ -1,53 +1,34 @@
---Nueva Calma
-local s,id=GetID()
+-- Nueva Calma
+local s,id = GetID()
 function s.initial_effect(c)
-    --Activate
+    -- Activar en respuesta a un ataque
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+    e1:SetCategory(CATEGORY_NEGATE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
-    e1:SetCost(s.cost)
+    e1:SetCondition(s.condition)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
 
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    Duel.Remove(g,POS_FACEUP,REASON_COST)
-    e:SetLabelObject(g:GetFirst())
+-- Condición para activar: tener un monstruo de Raza Guerrero en el cementerio
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttacker():IsControler(1-tp) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil)
 end
 
-function s.costfilter(c)
-    return c:IsRace(RACE_WARRIOR) and c:IsAbleToRemoveAsCost()
+-- Filtro para buscar monstruos de Raza Guerrero en el cementerio
+function s.filter(c)
+    return c:IsRace(RACE_WARRIOR)
 end
 
+-- Objetivo de la carta: negar el ataque
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.NegateAttack() end
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
 
+-- Activación de la carta: cancelar el ataque
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.NegateAttack() then
-        local tc=e:GetLabelObject()
-        if tc and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-            Duel.BreakEffect()
-            Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_SET_ATTACK)
-            e1:SetValue(0)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-            tc:RegisterEffect(e1)
-            local e2=Effect.CreateEffect(e:GetHandler())
-            e2:SetType(EFFECT_TYPE_SINGLE)
-            e2:SetCode(EFFECT_SET_DEFENSE)
-            e2:SetValue(0)
-            e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-            tc:RegisterEffect(e2)
-        end
-    end
+    Duel.NegateAttack()
 end
