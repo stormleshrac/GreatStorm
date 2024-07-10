63998274f6e1674a5fe1442d73ff39c9a5cd6f54
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 12:28:13 -0400

    Add files via upload

---
+++ 1130.lua
@@ -0,0 +1,19 @@
+-- Crowa
+local s,id=GetID()
+function s.initial_effect(c)
+    -- activar en el campo
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DRAW)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Draw(tp,2,REASON_EFFECT)
+end
