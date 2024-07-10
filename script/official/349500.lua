e21c7d107634750c60058b5a1ef041f440cbe298
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 18:15:52 +0200

    Update 349500.lua

--- script/official/349500.lua
+++ script/official/349500.lua
@@ -1,7 +1,7 @@
---Nueva Calma
-local s,id = GetID()
+-- Nueva Calma
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Activar en respuesta a un ataque
+    -- activar y negar ataque
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_NEGATE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -11,24 +11,13 @@ function s.initial_effect(c)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
-
--- Condición para activar: tener un monstruo de Raza Guerrero en el cementerio
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetAttacker():IsControler(1-tp) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil)
+    return tp~=Duel.GetTurnPlayer()
 end
-
--- Filtro para buscar monstruos de Raza Guerrero en el cementerio
-function s.filter(c)
-    return c:IsRace(RACE_WARRIOR)
-end
-
--- Objetivo de la carta: negar el ataque
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
     Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
-
--- Activación de la carta: cancelar el ataque
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     Duel.NegateAttack()
 end
