79a93780ee41ab70b0fde5fb9713bbc63d2418b9
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 10:52:36 -0400

    Update 349500.lua

--- script/official/349500.lua
+++ script/official/349500.lua
@@ -1,52 +1,48 @@
 --Nueva Calma
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
+    -- Activate
     local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_NEGATE+CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCondition(s.condition)
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
 
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    if #g>0 then
-        Duel.Remove(g,POS_FACEUP,REASON_COST)
-        e:SetLabelObject(g:GetFirst())
-    end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttacker():IsControler(1-tp)
 end
 
-function s.costfilter(c)
-    return c:IsRace(RACE_WARRIOR) and c:IsAbleToRemoveAsCost()
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsRace,tp,LOCATION_HAND,0,1,nil,RACE_WARRIOR) end
+    Duel.DiscardHand(tp,Card.IsRace,1,1,REASON_COST+REASON_DISCARD,nil,RACE_WARRIOR)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.NegateAttack() end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_REMOVED)
+    if chk==0 then return Duel.NegateAttack() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     if Duel.NegateAttack() then
-        local tc=e:GetLabelObject()
-        if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+        Duel.BreakEffect()
+        local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(Card.IsRace),tp,LOCATION_GRAVE,0,1,1,nil,RACE_WARRIOR)
+        if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
+            local tc=g:GetFirst()
             local e1=Effect.CreateEffect(e:GetHandler())
             e1:SetType(EFFECT_TYPE_SINGLE)
             e1:SetCode(EFFECT_SET_ATTACK_FINAL)
             e1:SetValue(0)
             e1:SetReset(RESET_EVENT+RESETS_STANDARD)
             tc:RegisterEffect(e1)
-            local e2=Effect.CreateEffect(e:GetHandler())
-            e2:SetType(EFFECT_TYPE_SINGLE)
+            local e2=e1:Clone()
             e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
-            e2:SetValue(0)
-            e2:SetReset(RESET_EVENT+RESETS_STANDARD)
             tc:RegisterEffect(e2)
         end
     end
