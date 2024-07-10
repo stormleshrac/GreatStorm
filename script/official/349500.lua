3e8dfa13bef2e0e7071a59799515e82aa43124c3
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 10:10:47 -0400

    Add files via upload

---
+++ script/official/349500.lua
@@ -0,0 +1,56 @@
+-- Nueva Calma
+local s,id = GetID()
+function s.initial_effect(c)
+    -- Activar
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCondition(s.condition)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+end
+s.listed_series={0x107a}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return tp~=Duel.GetTurnPlayer()
+end
+function s.cfilter(c,e,tp)
+    return c:IsRace(RACE_WARRIOR) and c:IsAbleToRemoveAsCost()
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+    e:SetLabel(g:GetFirst():GetCode())
+end
+function s.spfilter(c,e,tp,code)
+    return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateAttack() then
+        if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,e:GetLabel())
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+            local tc=g:GetFirst()
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+            e1:SetValue(0)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+            tc:RegisterEffect(e1)
+            local e2=e1:Clone()
+            e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+            tc:RegisterEffect(e2)
+        end
+    end
+end
