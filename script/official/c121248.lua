c68832e0adcf3edf225637661a4efae332a438e6
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-10 14:24:46 +0200

    Update c121248.lua

--- script/official/c121248.lua
+++ script/official/c121248.lua
@@ -1,1 +1,115 @@
-
+--Oculus el Majestuoso
+local s,id=GetID()
+function s.initial_effect(c)
+    --Link summon method
+    c:EnableReviveLimit()
+    Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),2,2,s.lcheck)
+    --Negate effect
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_NEGATE)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetCondition(s.negcon)
+    e1:SetTarget(s.negtg)
+    e1:SetOperation(s.negop)
+    c:RegisterEffect(e1)
+    --Direct attack
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_DIRECT_ATTACK)
+    e2:SetCondition(s.dircon)
+    e2:SetOperation(s.dirop)
+    c:RegisterEffect(e2)
+    --Reduce ATK
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e3:SetCondition(s.atkcon)
+    e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
+    --Negate effect of opponent's monster
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,3))
+    e4:SetCategory(CATEGORY_DISABLE)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.distg)
+    e4:SetOperation(s.disop)
+    c:RegisterEffect(e4)
+end
+function s.lcheck(g,lc,sumtype,tp)
+    return g:IsExists(Card.IsType,1,nil,TYPE_FUSION,lc,sumtype,tp) and g:IsExists(Card.IsRace,1,nil,RACE_WARRIOR,lc,sumtype,tp)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep~=tp
+        and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev) and Duel.GetTurnPlayer()==tp
+        and not e:GetHandler():GetFlagEffect(id)>0
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateActivation(ev) then
+        local c=e:GetHandler()
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_CANNOT_ATTACK)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
+        c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+    end
+end
+function s.dircon(e)
+    return not e:GetHandler():IsHasEffect(EFFECT_CANNOT_ATTACK)
+end
+function s.dirop(e,tp,eg,ep,ev,re,r,rp)
+    e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetAttackTarget()==nil and e:GetHandler():IsHasEffect(EFFECT_DIRECT_ATTACK)
+        and not e:GetHandler():GetFlagEffect(id)>0
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-1000)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
+        c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+    end
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+        Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_CANNOT_ATTACK)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e2)
+    end
+end
