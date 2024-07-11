b00a03fd0557aca6f4c3b3dcc56a475077b68f4c
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-11 16:45:56 +0200

    Update c2397.lua

--- script/official/c2397.lua
+++ script/official/c2397.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
     e1:SetTarget(s.eqtg)
     e1:SetOperation(s.eqop)
     c:RegisterEffect(e1)
-    -- Efecto de protección
+    -- Efecto de no destrucciÃ³n
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_EQUIP)
     e2:SetCode(EFFECT_INDESTRUCTIBLE_EFFECT)
@@ -19,7 +19,7 @@ end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
         and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
-    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_MZONE)
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
