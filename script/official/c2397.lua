0b332c5826a268bd01a121333db5ddc7dd632340
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-11 10:41:23 -0400

    Add files via upload

---
+++ script/official/c2397.lua
@@ -0,0 +1,33 @@
+-- Dracoescudo Rrar
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Efecto de equipar
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
+    c:RegisterEffect(e1)
+    -- Efecto de protección
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_EQUIP)
+    e2:SetCode(EFFECT_INDESTRUCTIBLE_EFFECT)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+    Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_MZONE)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,c)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.Equip(tp,c,tc)
+    end
+end
