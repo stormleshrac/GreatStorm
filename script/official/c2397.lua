361f9f98e68526b8049bce76f42002eb6b4375fb
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-11 17:10:05 +0200

    Update c2397.lua

--- script/official/c2397.lua
+++ script/official/c2397.lua
@@ -1,54 +1,66 @@
--- Dracoescudo Rrar
-local s,id=GetID()
+--Dracoescudo Rrar
+local s, id = GetID()
 function s.initial_effect(c)
-    -- Efecto de equipar
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
+    -- Effect: Discard and equip to another monster
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_EQUIP)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1)
-    e1:SetTarget(s.eqtg)
-    e1:SetOperation(s.eqop)
+    e1:SetCost(s.equipcost)
+    e1:SetTarget(s.equiptg)
+    e1:SetOperation(s.equipop)
     c:RegisterEffect(e1)
-    -- Efecto de inmunidad
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_EQUIP)
-    e2:SetCode(EFFECT_IMMUNE_EFFECT)
-    e2:SetValue(s.efilter)
+
+    -- Equip limit
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_EQUIP_LIMIT)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetValue(s.eqlimit)
     c:RegisterEffect(e2)
+
+    -- Immune to effects when attacking
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_EQUIP)
+    e3:SetCode(EFFECT_IMMUNE_EFFECT)
+    e3:SetValue(s.efilter)
+    e3:SetCondition(s.imcon)
+    c:RegisterEffect(e3)
 end
-s.listed_series={0xff}
-function s.filter(c)
-    return c:IsFaceup() and c:IsSetCard(0xff)
+
+function s.eqlimit(e, c)
+    return c:IsFaceup()
 end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-        and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-    Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+
+function s.equipcost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable, tp, LOCATION_HAND, 0, 1, nil) end
+    Duel.DiscardHand(tp, Card.IsDiscardable, 1, 1, REASON_COST + REASON_DISCARD)
 end
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp) then
-        if Duel.Equip(tp,e:GetHandler(),tc) then
-            -- Añade un efecto adicional de equipamiento
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
-            e1:SetCode(EFFECT_EQUIP_LIMIT)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-            e1:SetValue(s.eqlimit)
-            e1:SetLabelObject(tc)
-            e:GetHandler():RegisterEffect(e1)
-        end
+
+function s.equiptg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+    if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_SZONE) > 0
+        and Duel.IsExistingTarget(Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, nil) end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_FACEUP)
+    local g = Duel.SelectTarget(tp, Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, 1, nil)
+    Duel.SetOperationInfo(0, CATEGORY_EQUIP, e:GetHandler(), 1, 0, 0)
+end
+
+function s.equipop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp) then
+        Duel.Equip(tp, c, tc)
     end
 end
-function s.eqlimit(e,c)
-    return c==e:GetLabelObject()
+
+function s.efilter(e, re)
+    return e:GetHandlerPlayer() ~= re:GetOwnerPlayer()
 end
-function s.efilter(e,re)
-    return e:GetHandlerPlayer()~=re:GetOwnerPlayer() and re:IsActivated() and re:IsActiveType(TYPE_MONSTER)
+
+function s.imcon(e)
+    return e:GetHandler():GetEquipTarget() and e:GetHandler():GetEquipTarget():IsAttackPos()
 end
