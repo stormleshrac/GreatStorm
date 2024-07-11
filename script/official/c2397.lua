31ef6b431133ceb5de8fc19bdacb086e39957a1b
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-11 17:01:58 +0200

    Update c2397.lua

--- script/official/c2397.lua
+++ script/official/c2397.lua
@@ -1,34 +1,54 @@
---Dracoescudo Rrar
+-- Dracoescudo Rrar
 local s,id=GetID()
 function s.initial_effect(c)
-    --Efecto de invocación
+    -- Efecto de equipar
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.operation)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_EQUIP)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
     c:RegisterEffect(e1)
+    -- Efecto de inmunidad
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_EQUIP)
+    e2:SetCode(EFFECT_IMMUNE_EFFECT)
+    e2:SetValue(s.efilter)
+    c:RegisterEffect(e2)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) and Duel.IsPlayerCanDiscardDeck(tp,1) end
+s.listed_series={0xff}
+function s.filter(c)
+    return c:IsFaceup() and c:IsSetCard(0xff)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-    local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
-    if #g>0 then
-        Duel.HintSelection(g)
-        local tc=g:GetFirst()
-        if Duel.DiscardHand(tp,1,1,REASON_EFFECT)>0 then
-            --Efecto de inmunidad
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+        and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(tp) then
+        if Duel.Equip(tp,e:GetHandler(),tc) then
+            -- Añade un efecto adicional de equipamiento
             local e1=Effect.CreateEffect(e:GetHandler())
             e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_IMMUNE_EFFECT)
-            e1:SetValue(s.efilter)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-            tc:RegisterEffect(e1)
+            e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
+            e1:SetCode(EFFECT_EQUIP_LIMIT)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+            e1:SetValue(s.eqlimit)
+            e1:SetLabelObject(tc)
+            e:GetHandler():RegisterEffect(e1)
         end
     end
 end
-function s.efilter(e,te)
-    return te:GetOwnerPlayer()~=e:GetHandlerPlayer() and te:IsActivated() and te:GetOwner():IsType(TYPE_MONSTER)
+function s.eqlimit(e,c)
+    return c==e:GetLabelObject()
+end
+function s.efilter(e,re)
+    return e:GetHandlerPlayer()~=re:GetOwnerPlayer() and re:IsActivated() and re:IsActiveType(TYPE_MONSTER)
 end
