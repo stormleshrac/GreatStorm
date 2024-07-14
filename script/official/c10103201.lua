e1f6a93df6674bfd528a12bbf8d583ee4dc68c1e
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-14 18:57:02 -0500

    Update c10103201.lua

--- script/official/c10103201.lua
+++ script/official/c10103201.lua
@@ -1,14 +1,14 @@
 -- Rodo
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Efecto de invocación
+    -- Invocación normal o especial
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetCountLimit(1,id)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
     local e2=e1:Clone()
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -16,25 +16,26 @@ function s.initial_effect(c)
 end
 s.listed_names={10103200}
 
-function s.spfilter(c,e,tp)
+function s.filter(c,e,tp)
     return c:IsCode(10103200) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local g=Duel.GetDecktopGroup(tp,5)
+        return g:IsExists(s.filter,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    end
 end
 
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.ConfirmDecktop(tp,5)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetDecktopGroup(tp,5)
-    if g:IsExists(s.spfilter,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+    Duel.ConfirmCards(tp,g)
+    if g:IsExists(s.filter,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=g:FilterSelect(tp,s.spfilter,1,1,nil,e,tp)
-        if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)~=0 then
-            Duel.BreakEffect()
-            Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT)
+        local sg=g:FilterSelect(tp,s.filter,1,1,nil,e,tp)
+        if #sg>0 then
+            Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+            Duel.Destroy(e:GetHandler(),REASON_EFFECT)
         end
     end
     Duel.ShuffleDeck(tp)
