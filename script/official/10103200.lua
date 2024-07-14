26d0043f4db96f8de12308782cb11bd5412c1075
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-14 19:11:56 -0400

    Add files via upload

---
+++ script/official/10103200.lua
@@ -0,0 +1,35 @@
+-- Super Rodo
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Efecto de invocación especial
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE+CATEGORY_RECOVER)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.destg)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+    if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        if Duel.Destroy(tc,REASON_EFFECT)~=0 then
+            if tc:IsType(TYPE_MONSTER) then
+                Duel.Recover(tp,tc:GetAttack()/2,REASON_EFFECT)
+            else
+                Duel.Damage(1-tp,e:GetHandler():GetAttack()/2,REASON_EFFECT)
+            end
+        end
+    end
+end
