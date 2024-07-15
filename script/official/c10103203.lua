c2128188c07231c99351786300c3088e7f93cf35
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-15 19:04:29 +0200

    Update c10103203.lua

--- script/official/c10103203.lua
+++ script/official/c10103203.lua
@@ -3,29 +3,23 @@ local s,id=GetID()
 function s.initial_effect(c)
     -- Cannot be Normal Summoned/Set
     c:EnableUnsummonable()
-    -- Special summon condition
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    e1:SetValue(aux.FALSE)
-    c:RegisterEffect(e1)
     -- Special summon rule
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_SPSUMMON_PROC)
-    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e2:SetRange(LOCATION_HAND)
-    e2:SetCondition(s.spcon)
-    c:RegisterEffect(e2)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.spcon)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
     -- Destroy monsters
-    local e3=Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_DESTROY)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e3:SetTarget(s.destg)
-    e3:SetOperation(s.desop)
-    c:RegisterEffect(e3)
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetTarget(s.destg)
+    e2:SetOperation(s.desop)
+    c:RegisterEffect(e2)
 end
 
 function s.spfilter(c)
@@ -34,7 +28,14 @@ end
 
 function s.spcon(e,c)
     if c==nil then return true end
-    return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+    local tp=c:GetControler()
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+    Duel.Release(tc,REASON_COST)
 end
 
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
