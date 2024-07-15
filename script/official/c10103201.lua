c922d9dd24ce03e7b6f27e42c588d9ad26c2923c
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-15 17:45:13 +0200

    Update c10103201.lua

--- script/official/c10103201.lua
+++ script/official/c10103201.lua
@@ -1,44 +1,40 @@
--- Rodo
-local s,id = GetID()
+--Rodo
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Efecto de invocación
+    -- Activate effect when summoned
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetCountLimit(1,id)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetTarget(s.target)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
-end
-s.listed_names={10103200}
-
-function s.filter(c)
-    return c:IsCode(10103200) and c:IsAbleToHand()
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=e1:Clone()
+    e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e3)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetDecktopGroup(tp,5):IsExists(s.filter,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,5) end
+    Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,5)
 end
 
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetDecktopGroup(tp,5)
-    if #g>0 then
-        Duel.ConfirmCards(tp,g)
-        if g:IsExists(s.filter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-            local sg=g:Filter(s.filter,nil)
-            if #sg>0 then
-                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-                local tg=sg:Select(tp,1,1,nil)
-                if Duel.SendtoHand(tg,nil,REASON_EFFECT)~=0 then
-                    Duel.ConfirmCards(1-tp,tg)
-                    Duel.ShuffleHand(tp)
-                    Duel.BreakEffect()
-                    Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+    if Duel.DiscardDeck(tp,5,REASON_EFFECT)~=0 then
+        local g=Duel.GetOperatedGroup()
+        if g:IsExists(Card.IsCode,1,nil,10103200) then
+            Duel.BreakEffect()
+            local c=e:GetHandler()
+            if c:IsRelateToEffect(e) and Duel.Destroy(c,REASON_EFFECT)~=0 then
+                local tg=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_GRAVE,0,nil,10103200)
+                if #tg>0 then
+                    Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP)
                 end
             end
         end
-        Duel.ShuffleDeck(tp)
     end
 end
