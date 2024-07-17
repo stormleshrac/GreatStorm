c28c2bbec6438bf481a5197930740b2f9eb9192a
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-17 15:01:39 +0200

    Update c10429902.lua

--- script/official/c10429902.lua
+++ script/official/c10429902.lua
@@ -1,3 +1,4 @@
+-- Wandawais
 local s,id=GetID()
 function s.initial_effect(c)
     -- Activate
@@ -5,6 +6,7 @@ function s.initial_effect(c)
     e1:SetCategory(CATEGORY_HANDES+CATEGORY_DESTROY)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
@@ -12,42 +14,31 @@ end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,0,LOCATION_HAND,1,nil)
-        and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,1)
+        and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,1-tp,1)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    -- You discard a card from your opponent's hand
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-    local g1=Duel.SelectMatchingCard(tp,Card.IsDiscardable,tp,0,LOCATION_HAND,1,1,nil)
-    if #g1>0 then
-        Duel.SendtoGrave(g1,REASON_DISCARD+REASON_EFFECT)
-        local dg=g1:GetFirst()
-        if dg:IsType(TYPE_MONSTER) then
-            if Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) then
-                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-                local mg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
-                if #mg>0 then
-                    Duel.Destroy(mg,REASON_EFFECT)
-                end
-            end
+    local g=Duel.SelectMatchingCard(tp,Card.IsDiscardable,tp,0,LOCATION_HAND,1,1,nil)
+    if #g>0 and Duel.SendtoGrave(g,REASON_DISCARD+REASON_EFFECT)>0 and g:GetFirst():IsType(TYPE_MONSTER) then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+        local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+        if #dg>0 then
+            Duel.Destroy(dg,REASON_EFFECT)
         end
     end
 
-    -- Opponent discards a card from your hand
+    if Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 then return end
     Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DISCARD)
-    local g2=Duel.SelectMatchingCard(1-tp,Card.IsDiscardable,tp,LOCATION_HAND,0,1,1,nil)
-    if #g2>0 then
-        Duel.SendtoGrave(g2,REASON_DISCARD+REASON_EFFECT)
-        local dg2=g2:GetFirst()
-        if dg2:IsType(TYPE_MONSTER) then
-            if Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_MZONE,0,1,nil) then
-                Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DESTROY)
-                local mg2=Duel.SelectMatchingCard(1-tp,aux.TRUE,1-tp,LOCATION_MZONE,0,1,1,nil)
-                if #mg2>0 then
-                    Duel.Destroy(mg2,REASON_EFFECT)
-                end
-            end
+    local hg=Duel.SelectMatchingCard(1-tp,Card.IsType,tp,LOCATION_HAND,0,1,1,nil)
+    Duel.ConfirmCards(tp,hg)
+    if hg:GetFirst():IsType(TYPE_MONSTER) then
+        Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DESTROY)
+        local dg2=Duel.SelectMatchingCard(1-tp,aux.TRUE,tp,LOCATION_MZONE,0,1,1,nil)
+        if #dg2>0 then
+            Duel.Destroy(dg2,REASON_EFFECT)
         end
     end
+    Duel.ShuffleHand(tp)
 end
