b5c23a21d58d033dfccbe30771dfbba6e03b03f3
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-19 17:27:58 +0200

    Update and rename c14601462.lua to c14601562.lua

--- script/official/c14601562.lua
+++ script/official/c14601562.lua
@@ -1,7 +1,7 @@
 -- Alraptor Magora
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Invocación especial desde la mano
+    -- InvocaciÃ³n especial desde la mano
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
