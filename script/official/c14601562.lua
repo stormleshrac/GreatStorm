29f89049db357273a4cc747b648415e0a48a42f1
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-19 11:46:30 -0400

    Update c14601562.lua

--- script/official/c14601562.lua
+++ script/official/c14601562.lua
@@ -1,22 +1,23 @@
--- Alraptor Magora
+--Alraptor Magora
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Invocación especial desde la mano
+    --Special summon from hand
     local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(s.spcon)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
     local e2=e1:Clone()
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e2)
-    -- Cambiar nivel
+    
+    --Change level
     local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
     e3:SetType(EFFECT_TYPE_IGNITION)
     e3:SetRange(LOCATION_MZONE)
     e3:SetCountLimit(1)
@@ -24,32 +25,41 @@ function s.initial_effect(c)
     e3:SetOperation(s.lvop)
     c:RegisterEffect(e3)
 end
-s.listed_series={0x7a0}
 
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(Card.IsSetCard,1,nil,0x7a0)
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0x7a0) and c:IsLevelBelow(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+    if g:GetCount()>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end
-function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_MZONE,0,1,nil,0x7a0) end
+
+function s.lvfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x7a0) and not c:IsType(TYPE_XYZ)
 end
+
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.lvfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    local g=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_MZONE,0,1,1,nil,0x7a0)
-    if #g>0 then
-        Duel.HintSelection(g)
-        local op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-        local tc=g:GetFirst()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    local g=Duel.SelectMatchingCard(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+        local op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4))
         if op==0 then
             local e1=Effect.CreateEffect(e:GetHandler())
             e1:SetType(EFFECT_TYPE_SINGLE)
