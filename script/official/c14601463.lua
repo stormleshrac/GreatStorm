cf671b0cba84edac081fea193f418c510b314be6
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-20 13:59:03 -0500

    Update c14601463.lua

--- script/official/c14601463.lua
+++ script/official/c14601463.lua
@@ -1,96 +1,79 @@
 --Alraptor Voraggro Amion
 local s,id=GetID()
 function s.initial_effect(c)
-    --Xyz Summon
-    Xyz.AddProcedure(c,nil,4,2,s.ovfilter,aux.Stringid(id,0))
+    --xyz summon
+    Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x7a0),4,2)
     c:EnableReviveLimit()
+    
     --Swap ATK and DEF
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE+CATEGORY_DESTROY)
     e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetRange(LOCATION_MZONE)
     e1:SetCountLimit(1)
     e1:SetCost(s.cost)
     e1:SetTarget(s.target)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
-    --Return material
+    
+    --Recover material
     local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_LEAVE_GRAVE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_BATTLE_DESTROYING)
-    e2:SetCondition(s.retcon)
-    e2:SetOperation(s.retop)
+    e2:SetCondition(s.reccon)
+    e2:SetOperation(s.recop)
+    e2:SetLabelObject(e1)
     c:RegisterEffect(e2)
-end
-
-function s.ovfilter(c,tp,xyzc)
-    return c:IsFaceup() and c:IsSetCard(0x7a0) and c:IsType(TYPE_MONSTER)
 end
 
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
     e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-    e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,0,1)
-end
-
-function s.filter(c)
-    return c:IsFaceup()
+    e:SetLabel(Duel.GetOperatedGroup():GetFirst():GetCode())
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-    local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+    if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 end
 
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local tc=Duel.GetFirstTarget()
-    if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+    local tc=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+    if not tc then return end
+    if c:IsRelateToEffect(e) and tc:IsFaceup() then
         local atk=tc:GetAttack()
         local def=tc:GetDefense()
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_SET_ATTACK_FINAL)
         e1:SetValue(def)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
         tc:RegisterEffect(e1)
         local e2=Effect.CreateEffect(c)
         e2:SetType(EFFECT_TYPE_SINGLE)
         e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
         e2:SetValue(atk)
-        e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD)
         tc:RegisterEffect(e2)
-        -- Store the target for the return material effect
-        c:RegisterFlagEffect(id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-        c:RegisterFlagEffect(id+2,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-        c:RegisterFlagEffect(tc:GetFieldID(),RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
     end
+    e:SetLabelObject(tc)
 end
 
-function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local tc=c:GetBattleTarget()
-    return c:GetFlagEffect(id+1)~=0 and tc and tc:IsType(TYPE_MONSTER) and tc:IsReason(REASON_BATTLE) and c:GetFlagEffect(tc:GetFieldID())~=0
+    local bc=c:GetBattleTarget()
+    local tc=e:GetLabelObject():GetLabelObject()
+    return bc and bc==tc and bc:IsLocation(LOCATION_GRAVE)
 end
 
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    Duel.Hint(HINT_CARD,0,id)
+    local code=e:GetLabelObject():GetLabel()
     if c:IsRelateToBattle() then
-        local og=Duel.GetOperatedGroup()
-        local rc=og:GetFirst()
-        while rc do
-            if rc:IsReason(REASON_BATTLE) and rc:IsPreviousLocation(LOCATION_MZONE) then
-                c:AddCounter(0x1041,1)
-                if Duel.SendtoGrave(rc,REASON_EFFECT+REASON_RETURN)~=0 then
-                    c:Overlay(og)
-                end
-            end
-            rc=og:GetNext()
-        end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+        Duel.Overlay(c,Group.FromCards(Duel.GetFirstMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,nil,code)))
     end
 end
