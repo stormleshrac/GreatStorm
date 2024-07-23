a2f417901da66197158c0b76bb8d55cc4d22a624
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-07-23 18:12:57 -0500

    Update c140601466.lua

--- script/official/c140601466.lua
+++ script/official/c140601466.lua
@@ -1,34 +1,47 @@
--- Carga Imbatible
-local s, id = GetID()
+--Carga Imbatible
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Activate
-    local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DISABLE)
+    --Activate
+    local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetCode(EVENT_ATTACK_DISABLED)
     e1:SetCondition(s.condition)
     e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
 
+function s.cfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x7a0) and c:IsLocation(LOCATION_MZONE)
+end
+
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    local atk=Duel.GetAttacker()
-    local tc=Duel.GetAttackTarget()
-    return atk and tc and atk:IsSetCard(0x7a0) and tc:IsControler(1-tp)
+    local tc=Duel.GetAttacker()
+    return tc and s.cfilter(tc) and tc:IsControler(tp)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(aux.disfilter1,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,0,0)
+    if chk==0 then return true end
+    local tc=Duel.GetAttacker()
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,tc,1,0,0)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.SelectMatchingCard(tp,aux.disfilter1,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil):GetFirst()
-    if tc and Duel.NegateEffect(tc:GetOriginalCode()) then
-        local atk=Duel.GetAttacker()
-        if atk:IsRelateToBattle() and not atk:IsImmuneToEffect(e) then
-            Duel.ChangeAttackTarget(atk)
-        end
+    local tc=Duel.GetAttacker()
+    if tc and tc:IsRelateToBattle() and tc:IsControler(tp) then
+        -- Negate effects that would negate the attack
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_DISABLE)
+        e1:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
+        e1:SetTarget(s.distg)
+        e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+        Duel.RegisterEffect(e1,tp)
+        -- Continue the attack
+        Duel.ContinueAttack()
     end
+end
+
+function s.distg(e,c)
+    return c:IsType(TYPE_TRAP+TYPE_SPELL+TYPE_MONSTER) and aux.disfilter1(c)
 end
