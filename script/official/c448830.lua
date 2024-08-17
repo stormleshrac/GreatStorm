234282ce886536c86f60944914cd3324979d5e99
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-08-17 08:26:10 -0500

    Update c448830.lua

--- script/official/c448830.lua
+++ script/official/c448830.lua
@@ -1,42 +1,44 @@
 --Rey Sortija
-local s, id = GetID()
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Effect when summoned
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_DISABLE_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    --Efecto al invocar
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
-    local e2 = e1:Clone()
+    local e2=e1:Clone()
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e2)
-
-    -- Prevent opponent from summoning in attack position
-    local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_FIELD)
-    e3:SetCode(EFFECT_CANNOT_SUMMON)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetTargetRange(0, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE + LOCATION_REMOVED + LOCATION_EXTRA)
-    e3:SetCondition(s.condition)
-    e3:SetTarget(s.sumlimit)
-    c:RegisterEffect(e3)
-
-    local e4 = e3:Clone()
-    e4:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    c:RegisterEffect(e4)
 end
 
-function s.operation(e, tp, eg, ep, ev, re, r, rp)
-    -- Trigger the effect automatically
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsFaceup() then
+        --El adversario no puede invocar en posición de ataque
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_CANNOT_SUMMON)
+        e1:SetTargetRange(0,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA)
+        e1:SetTarget(s.sumlimit)
+        e1:SetCondition(s.condition)
+        Duel.RegisterEffect(e1,tp)
+        
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_FIELD)
+        e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+        e2:SetTargetRange(0,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_EXTRA)
+        e2:SetTarget(s.sumlimit)
+        e2:SetCondition(s.condition)
+        Duel.RegisterEffect(e2,tp)
+    end
 end
 
 function s.condition(e)
-    return e:GetHandler():IsFaceup()
+    return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,e:GetHandler():GetCode()),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 end
 
-function s.sumlimit(e, c, sump, sumtype, sumpos, targetp)
-    return sumpos == POS_FACEUP_ATTACK
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+    return sumpos&POS_FACEUP_ATTACK>0
 end
