ea2495b1564d26b572855d06860fa65cb2007605
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2024-11-30 16:54:28 +0100

    Update c448830.lua

--- script/official/c448830.lua
+++ script/official/c448830.lua
@@ -1,38 +1,23 @@
---Rey Sortija
+-- Rey Sortija
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activar efecto al invocarse
+    -- cannot summon in attack position
     local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetOperation(s.activate)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_SUMMON)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(0,1)
+    e1:SetCondition(s.condition)
+    e1:SetTarget(s.sumlimit)
     c:RegisterEffect(e1)
     local e2=e1:Clone()
-    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
     c:RegisterEffect(e2)
 end
 
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsFaceup() then
-        --El adversario no puede invocar monstruos en posición de ataque
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_FIELD)
-        e1:SetCode(EFFECT_CANNOT_SUMMON)
-        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        e1:SetTargetRange(0,1)
-        e1:SetTarget(s.sumlimit)
-        Duel.RegisterEffect(e1,tp)
-        
-        local e2=Effect.CreateEffect(c)
-        e2:SetType(EFFECT_TYPE_FIELD)
-        e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-        e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        e2:SetTargetRange(0,1)
-        e2:SetTarget(s.sumlimit)
-        Duel.RegisterEffect(e2,tp)
-    end
+function s.condition(e)
+    return e:GetHandler():IsFaceup()
 end
 
 function s.sumlimit(e,c)
